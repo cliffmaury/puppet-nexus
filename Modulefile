8e03f2a959c35343e8c88c82a66a69f72bf01bc3
Cliff Maury <cliff.maury@gmail.com>
2011-11-18 20:58:33 +0100

    Initial commit for submodule puppet-nexus from Geppeto

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'cliff-puppet-nexus'
+version '0.0.1'
+source ''
+author 'cliff'
+license ''
+summary ''
+description ''
+project_page ''
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: puppet-nexus
+#
+# This module manages puppet-nexus
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class puppet-nexus {
+
+
+}
